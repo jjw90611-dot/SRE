4d5b26c94f33dd9e388f8e5a6f0d9d38380b4f5b
jjw90611-dot <jjw90611@gmail.com>
2026-06-02 11:34:59 +0900

    Update SRE.py

--- SRE.py
+++ SRE.py
@@ -1,42 +1,18 @@
 import streamlit as st
-import streamlit.components.v1 as components
+import folium
+from streamlit_folium import st_folium
 
-st.title("카카오 맵 테스트")
+st.title("Folium 지도 테스트")
 
-# Secrets에서 키 가져오기
-api_key = st.secrets["KAKAO_API_KEY"]
+# 1. 지도 객체 생성 (서울시청 좌표)
+m = folium.Map(location=[37.566826, 126.9786567], zoom_start=15)
 
-# 지도만 띄우는 가장 단순하고 확실한 HTML/JS 코드
-map_html = f"""
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-</head>
-<body>
-    <div id="map" style="width:100%;height:400px;"></div>
-    
-    <script>
-        // 1. 스크립트 동적 로드 (Streamlit 환경에서 가장 안전한 방식)
-        var script = document.createElement('script');
-        script.src = "https://dapi.kakao.com/v2/maps/sdk.js?appkey={api_key}&autoload=false";
-        document.head.appendChild(script);
+# 2. 마커 추가
+folium.Marker(
+    [37.566826, 126.9786567], 
+    popup="서울시청", 
+    tooltip="클릭해보세요"
+).add_to(m)
 
-        // 2. 스크립트 로드가 끝나면 지도 그리기
-        script.onload = function() {{
-            kakao.maps.load(function() {{
-                var mapContainer = document.getElementById('map');
-                var mapOption = {{
-                    center: new kakao.maps.LatLng(37.566826, 126.9786567), // 서울시청 좌표
-                    level: 3
-                }};
-                var map = new kakao.maps.Map(mapContainer, mapOption);
-            }});
-        }};
-    </script>
-</body>
-</html>
-"""
-
-# 화면에 출력
-components.html(map_html, height=450)
+# 3. Streamlit 화면에 출력
+st_folium(m, width=700, height=500)
