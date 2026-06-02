3f42e3a0b753a6c84b6e5b6dc345b4712f1709db
jjw90611-dot <jjw90611@gmail.com>
2026-06-02 11:27:30 +0900

    Update SRE.py

--- SRE.py
+++ SRE.py
@@ -10,27 +10,32 @@ except KeyError:
     st.error("Streamlit Cloud Secrets에 'KAKAO_API_KEY'가 설정되지 않았습니다.")
     st.stop()
 
-# 2. 카카오맵 HTML 코드 (autoload=false 및 kakao.maps.load 적용)
+# 2. 카카오맵 HTML 코드 (동적 스크립트 로딩 방식)
 map_html = f"""
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
-    <!-- 파이썬에서 불러온 API 키를 주입하고 autoload=false 설정 -->
-    <script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey={kakao_api_key}&autoload=false"></script>
 </head>
 <body>
     <div id="map" style="width:100%;height:400px;"></div>
     <script>
-        // 카카오맵 SDK가 완전히 로드된 후 실행되도록 보장
-        kakao.maps.load(function() {{
-            var container = document.getElementById('map');
-            var options = {{
-                center: new kakao.maps.LatLng(37.566826, 126.9786567), // 기본 좌표 (서울시청)
-                level: 3
-            }};
-            var map = new kakao.maps.Map(container, options);
-        }});
+        // 1. 카카오맵 스크립트를 동적으로 생성하여 로드합니다.
+        var script = document.createElement('script');
+        script.src = "https://dapi.kakao.com/v2/maps/sdk.js?appkey={kakao_api_key}&autoload=false";
+        document.head.appendChild(script);
+
+        // 2. 스크립트 다운로드가 '완전히 끝난 후(onload)' 지도를 실행합니다.
+        script.onload = function() {{
+            kakao.maps.load(function() {{
+                var container = document.getElementById('map');
+                var options = {{
+                    center: new kakao.maps.LatLng(37.566826, 126.9786567),
+                    level: 3
+                }};
+                var map = new kakao.maps.Map(container, options);
+            }});
+        }};
     </script>
 </body>
 </html>
